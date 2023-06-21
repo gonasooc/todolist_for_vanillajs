b8620494db4df538b8ee2dd11372a86d542735b6
gonasooc <doupler@gmail.com>
2023-06-21 11:18:47 +0900

    날씨 api 호출하기

---
+++ assets/js/custom/weather.js
@@ -0,0 +1,33 @@
+const weather = document.querySelector('#weather');
+const API_KEY = '716b32cd762e891cc58d9d4e35c0d057'; // my key
+
+function getWeather(lat, long) {
+  console.log(lat, long);
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      const name = json.name;
+      const temp = json.main.temp.toFixed(0);
+      const humidity = json.main.humidity;
+      weather.innerHTML = `${temp}&#8451, ${humidity} @ ${name}`;
+    })
+}
+
+function handleGeoSuccess(pos) {
+  const latitude = pos.coords.latitude;
+  const longitude = pos.coords.longitude;
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log('error!');
+}
+
+function init() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+init();
